2ddecf2c191cc6fa164026408791b746b9e46af9
Mitchel Joy <mitchel_joy@hotmail.com>
2024-04-07 20:59:02 -0230

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Corrected import
 import { ShoppingCartProvider } from './components/ShoppingCartContext';
 import ShoppingCart from './components/ShoppingCart';
 import Checkout from './components/Checkout';
@@ -9,25 +9,24 @@ import Productlistmen from './components/productlistmen.jsx';
 import Productlistwomen from './components/productlistwomen.jsx';
 import Home from './components/home.jsx';
 import Footer from './components/footer.jsx';
-
+ 
 function App() {
-  return (
-    <BrowserRouter>
-    <ShoppingCartProvider>
-      <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/men" element={<Productlistmen />} />
-          <Route path="/women" element={<Productlistwomen />} />
-
-          <Route path="/cart" element={<ShoppingCart />} />
-          <Route path="/checkout" element={<Checkout />} />
-        </Routes>
-      <CartSidebar />
-      <Footer />
-    </ShoppingCartProvider>
-    </BrowserRouter>
-  );
-}
-
+    return (
+      <BrowserRouter>
+          <ShoppingCartProvider>
+              <Header />
+                  <Routes> {/* Correct use of Routes */}
+                      <Route path="/" element={<Home />} />
+                      <Route path="/men" element={<Productlistmen />} />
+                      <Route path="/women" element={<Productlistwomen />} />
+                      <Route path="/cart" element={<ShoppingCart />} />
+                      <Route path="/checkout" element={<Checkout />} />
+                  </Routes>
+              <CartSidebar />
+              <Footer />
+          </ShoppingCartProvider>
+      </BrowserRouter>
+    );
+  }
+  
 export default App;
